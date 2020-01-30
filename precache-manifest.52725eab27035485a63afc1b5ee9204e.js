4e00b7a9b57f60b576ac221ba9aa8beef94aa8a5
Jeremy Hall <Coldreactor64@gmail.com>
2020-01-30 08:46:31 -0600

    Updates

---
+++ precache-manifest.52725eab27035485a63afc1b5ee9204e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df6752b65771efbbed6b8a81b39478d",
+    "url": "/Breakout-Game-React/index.html"
+  },
+  {
+    "revision": "ad98376824154f2c044e",
+    "url": "/Breakout-Game-React/static/css/main.6735ed0c.chunk.css"
+  },
+  {
+    "revision": "0eb6d4c5ffda2670f6a3",
+    "url": "/Breakout-Game-React/static/js/2.eb186060.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Breakout-Game-React/static/js/2.eb186060.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ad98376824154f2c044e",
+    "url": "/Breakout-Game-React/static/js/main.60dbb24f.chunk.js"
+  },
+  {
+    "revision": "328d65578d34ed5c08f1",
+    "url": "/Breakout-Game-React/static/js/runtime-main.0b7f8618.js"
+  }
+]);
